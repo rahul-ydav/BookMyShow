0516bea6a461da7e052a4f63256311af568d670f
Mayank RSG <mayank.kumar@navigaglobal.com>
2023-11-04 17:38:58 +0530

    movie + language

---
+++ src/main/java/com/example/bookmyshow/model/Movie.java
@@ -0,0 +1,18 @@
+package com.example.bookmyshow.model;
+
+import com.example.bookmyshow.enums.Language;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Builder
+@Getter
+@Setter
+public class Movie {
+    private String name;
+    private Double rating;
+    private String genre;
+    private ArrayList<Language> languages;
+}

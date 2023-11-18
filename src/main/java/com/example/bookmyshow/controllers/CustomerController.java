df2ef1025b88b9caf5369426d52d7a0cb92a7e56
rahulydv <ry040799@gmail.com>
2023-11-18 15:17:47 +0530

    customer service

--- src/main/java/com/example/bookmyshow/controllers/CustomerController.java
+++ src/main/java/com/example/bookmyshow/controllers/CustomerController.java
@@ -3,20 +3,24 @@ package com.example.bookmyshow.controllers;
 import com.example.bookmyshow.dto.CreateCustomerDTO;
 import com.example.bookmyshow.exceptions.InvalidCustomerException;
 import com.example.bookmyshow.models.Customer;
+import com.example.bookmyshow.services.CustomerService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customer")
+@AllArgsConstructor
 public class CustomerController {
+    private CustomerService customerService;
     @GetMapping("/{id}")
     public Customer getCustomer(@PathVariable Long id){
-        return null;
+        return customerService.getCustomer(id);
     }
 
     @PostMapping
     public Customer postCustomer(@RequestBody CreateCustomerDTO request) {
         this.validate(request);
-        return null;
+        return customerService.createCustomer(request);
     }
 
     private void validate(CreateCustomerDTO request) {
---
+++ src/main/java/com/example/bookmyshow/services/CustomerService.java
@@ -0,0 +1,16 @@
+package com.example.bookmyshow.services;
+
+import com.example.bookmyshow.dto.CreateCustomerDTO;
+import com.example.bookmyshow.models.Customer;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+    public Customer getCustomer(Long id){
+        return null;
+    }
+
+    public Customer createCustomer(CreateCustomerDTO request){
+        return null;
+    }
+}
